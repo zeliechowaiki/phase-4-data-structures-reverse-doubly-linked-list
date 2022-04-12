55d6ef31c04af9d82bd672ec8d8675592cc48ee4
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-12 10:28:10 -0400

    commit tree 8ce6f694db9b4a401e63dbd7ee581bc0fe029abc

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,5 +10,15 @@ class LinkedList
 
   def reverse!
     # your code here
+    return nil if head.nil?
+    node = head.next_node ? head.next_node : head
+    while node.prev_node
+      node = node.prev_node
+      prev_copy = node.prev_node
+      node.prev_node = node.next_node
+      node.next_node = prev_copy
+    end
+    self.tail = head
+    self.head = node
   end
 end
